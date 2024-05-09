b0bcb90cd78472a1205aedc0b281397dae4c2761
Narendra Chatterjee <48941364+narendrachatterjee@users.noreply.github.com>
2024-05-10 01:00:53 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,17 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  const data = imageData();
+  return (
+    <div className='Home'>
+      <h1 className='head'>Kalvium Gallery</h1>
+      <div className='container-image'>
+        {data.map(item => (
+          <img key={item.id} src={item.img} alt={`Image ${item.id}`} className = 'image' />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,17 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render(){
+    const data = imageData();
+    return (
+      <div className='Home'>
+        <h1 className='head'>Kalvium Gallery</h1>
+        <div className='container-image'>
+          {data.map(item => (
+            <img key={item.id} src={item.img} alt={`Image ${item.id}`} className = 'image' />
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
